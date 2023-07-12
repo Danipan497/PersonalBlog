bcb684f981f87531d6bcf1d85750015642a6aa49
Danipan497 <37379987+Danipan497@users.noreply.github.com>
2023-07-12 15:12:45 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Models;
+
+namespace PersonalBlog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<PersonalBlog.Models.BlogPost>? BlogPost { get; set; }
+    }
+}
---
+++ Models/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonalBlog.Models
+{
+
+
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Entry { get; set; }
+        public string EntryContent { get; set; }
+
+        public BlogPost()
+        {
+            
+        }
+    }
+}
